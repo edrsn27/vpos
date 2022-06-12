96bf2743ca741c4af6669ec22700194530711097
ederson <edersonvillegas50@gmail.com>
2022-06-13 05:22:15 +0800

    initial signup

---
+++ pages/auth/sign-up.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SignUp from "../../components/Auth/SignIn";
+import Layout from "../../layouts/Auth";
+export default function signup() {
+  return (
+    <Layout>
+      <SignUp />
+    </Layout>
+  );
+}
