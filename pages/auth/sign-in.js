cd4439722d5925d4475a7a42d0b7765b2ccbc160
ederson <edersonvillegas50@gmail.com>
2022-06-13 05:20:42 +0800

    initial sign-in page

---
+++ pages/auth/sign-in.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SignIn from "../../components/Auth/SignIn";
+import Layout from "../../layouts/Auth";
+export default function signin() {
+  return (
+    <Layout>
+      <SignIn />
+    </Layout>
+  );
+}
