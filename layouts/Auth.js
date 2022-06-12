7bf87b1743689f5423b1c6d1fc94b2b60a982e3a
ederson <edersonvillegas50@gmail.com>
2022-06-13 03:30:55 +0800

    initial layout

---
+++ layouts/Auth.js
@@ -0,0 +1,11 @@
+import Header from "../components/Headers/Header";
+
+import React from "react";
+
+export default function Auth() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Layout from "../layouts/Auth"
 export default function index() {
-  return <></>;
+  return <Layout></Layout>;
 }
